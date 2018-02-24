b1b72b3999d32e6969aba523140dcc5d331d1f74
runformoney <36782664+runformoney@users.noreply.github.com>
2018-02-24 09:51:31 +0530

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -6,11 +6,14 @@ class DBHelper:
         # self.dbname = dbname
         global db
         global conn
-        db = MySQLdb.connect(host="172.30.76.229", port = 3306, user = "userCL4", passwd = "orVrVQUUJAG0nsQR", db = "sampledb",connect_timeout=28800)
+        db = MySQLdb.connect(host="172.30.76.229", port = 3306, user = "userCL4", passwd = "orVrVQUUJAG0nsQR", db = "sampledb")        
         #db = MySQLdb.connect(host="172.30.115.81", port=3306, user="userXGB", passwd=" qkBjP1g0RvBYu6QY", db="sampledb")
         #db = MySQLdb.connect(host="172.30.115.81", port=3306, user="userRTL", passwd="eNU1gYbC1EYLe6gN", db="sampledb")
         #db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="admin", db="sampledb")
         conn = db.cursor()
+        conn.query('SET GLOBAL connect_timeout=28800')
+        conn.query('SET GLOBAL wait_timeout=28800')
+        conn.query('SET GLOBAL interactive_timeout=28800')
         print("Connection with DB successfull")
 
         def setup(self):
