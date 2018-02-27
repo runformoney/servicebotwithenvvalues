1aa2849ac397caa6aae372425a2685337691e4b1
runformoney <36782664+runformoney@users.noreply.github.com>
2018-02-27 11:00:39 +0530

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -9,7 +9,7 @@ class DBHelper:
         #db = MySQLdb.connect(host="172.30.88.166", port = 3306, user = "userWIP", passwd = "atyKAFDkMW6dnMri", db = "sampledb")        
         #db = MySQLdb.connect(host="172.30.115.81", port=3306, user="userXGB", passwd=" qkBjP1g0RvBYu6QY", db="sampledb")
         #db = MySQLdb.connect(host="172.30.115.81", port=3306, user="userRTL", passwd="eNU1gYbC1EYLe6gN", db="sampledb")
-        db = MySQLdb.connect(host="172.30.203.197", port=3306, user="userAKD", passwd="PBdxsrmvNRbvvRrw", db="sampledb")
+        db = MySQLdb.connect(host="172.30.35.182", port=3306, user="userBW3", passwd="5AIKB00yB1FehAe4", db="sampledb")
         conn = db.cursor()
         #conn.query('SET GLOBAL connect_timeout=28800')
         #conn.execute('SET GLOBAL wait_timeout=28800')
@@ -32,7 +32,7 @@ class DBHelper:
     def db_connect(self):
         global db
         global conn
-        db = MySQLdb.connect(host="172.30.203.197", port=3306, user="userAKD", passwd="PBdxsrmvNRbvvRrw", db="sampledb")
+        db = MySQLdb.connect(host="172.30.35.182", port=3306, user="userBW3", passwd="5AIKB00yB1FehAe4", db="sampledb")
         conn = db.cursor()
 
     def add_item(self, item_text, owner):
